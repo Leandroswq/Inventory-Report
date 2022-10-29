c9f87bada5ff4713f3294f035e913e6c68063212
Leandroswq <leandrobonfim15@hotmail.com>
2022-10-28 21:11:02 -0300

    test: Testa a criação do produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,50 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+import datetime
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_data = {
+        "id": 0,
+        "nome_do_produto": "Azeitona",
+        "nome_da_empresa": "Empresa x",
+        "data_de_fabricacao": datetime.datetime(2020, 5, 17),
+        "data_de_validade": datetime.datetime(2020, 9, 17),
+        "numero_de_serie": 0,
+        "instrucoes_de_armazenamento": "Exemplo",
+    }
+
+    product = Product(
+        id=product_data["id"],
+        nome_do_produto=product_data["nome_do_produto"],
+        nome_da_empresa=product_data["nome_da_empresa"],
+        data_de_fabricacao=product_data["data_de_fabricacao"],
+        data_de_validade=product_data["data_de_validade"],
+        numero_de_serie=product_data["numero_de_serie"],
+        instrucoes_de_armazenamento=product_data[
+            "instrucoes_de_armazenamento"
+        ],
+    )
+
+    aux_instrucoes = product_data["instrucoes_de_armazenamento"]
+
+    product_repr = (
+        f"O produto {product_data['nome_do_produto']}"
+        f" fabricado em {product_data['data_de_fabricacao']}"
+        f" por {product_data['nome_da_empresa']} com validade"
+        f" até {product_data['data_de_validade']}"
+        f" precisa ser armazenado {aux_instrucoes}."
+    )
+
+    assert product.id == product_data["id"]
+    assert product.nome_do_produto == product_data["nome_do_produto"]
+    assert product.nome_da_empresa == product_data["nome_da_empresa"]
+    assert product.data_de_fabricacao == str(
+        product_data["data_de_fabricacao"]
+    )
+    assert product.data_de_validade == str(product_data["data_de_validade"])
+    assert product.numero_de_serie == product_data["numero_de_serie"]
+    assert (
+        product.instrucoes_de_armazenamento
+        == product_data["instrucoes_de_armazenamento"]
+    )
+    assert repr(product) == product_repr
