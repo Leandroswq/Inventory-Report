d55dd489bbd7a3147931bbd35d50cf052255c8df
Leandroswq <leandrobonfim15@hotmail.com>
2022-10-30 19:55:21 -0300

    feat: Gera um relatorio a partir de um arquivo xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,9 @@
 import csv
+import pathlib
+from typing import Dict, List
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
+import xmltodict
 
 
 class Inventory:
@@ -19,17 +22,38 @@ class Inventory:
         return inventory_list
 
     @staticmethod
+    def __load_data_xml__(path: str):
+        with open(path, "r") as file:
+            content = file.read()
+        data = xmltodict.parse(content)["dataset"]["record"]
+
+        return data
+
+    @staticmethod
+    def __generate_report__(data: List[Dict[str, str]], type: str):
+        if type == "simples":
+            return SimpleReport.generate(list=data)
+        elif type == "completo":
+            return CompleteReport.generate(list=data)
+
+        raise ValueError("Invalid type report")
+
+    @staticmethod
     def import_data(path: str, type: str):
         report_types = ["simples", "completo"]
 
         if type not in report_types:
+            raise ValueError("Invalid type report")
+
+        suffix = pathlib.Path(path).suffix
+
+        if suffix == ".csv":
+            inventory_list = Inventory.__load_data_csv__(path=path)
+        elif suffix == ".xml":
+            inventory_list = Inventory.__load_data_xml__(path=path)
+        else:
             raise ValueError(
-                "The parameter 'type' needs to be 'simples' or 'completo'"
+                f"This function is incompatible with {suffix} files"
             )
 
-        inventory_list = Inventory.__load_data_csv__(path=path)
-
-        if type == "simples":
-            return SimpleReport.generate(list=inventory_list)
-        elif type == "completo":
-            return CompleteReport.generate(list=inventory_list)
+        return Inventory.__generate_report__(data=inventory_list, type=type)
